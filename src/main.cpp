38af2b02d06371ae5be9e1f22db807f91d6a7e1e
Robin Hayman <r.hayman@ucl.ac.uk>
2021-01-25 19:24:36 +0000

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -20,20 +20,20 @@ int main(int argc, char const *argv[])
     }
     std::fstream infile{ argv[1] };
 
-    std::string line;
+    std::string line, output_dir;
+    // the first line should be the output directory so read that
+    std::getline(infile, output_dir);
     std::vector<std::string> file_list;
-    int count = 0;
     while(std::getline(infile, line)) {
-        if( does_file_exist(line) && count > 0 ) {
+        if( does_file_exist(line) ) {
             file_list.push_back(line);
-            ++count;
         }
-        else if ( count > 0 && ! does_file_exist(line)) {
+        else {
             std::cout << "File : " + line + " does not exist" << std::endl;
             return 1;
         }
     }
-
+    std::vector<std::string> output_files;
     for ( const auto & this_file : file_list ) {
         NwbData data{this_file};
         auto paths = data.getPaths();
@@ -48,7 +48,6 @@ int main(int argc, char const *argv[])
         // Check if there is more than one path to 'continuous' 'data' which might mean that there is 'recording0'
         // and 'recording1' and maybe more. Go with the one with the most amount of data in i.e. assume the user
         // erroneously pressed record and aborted...
-        std::string path2data2export;
         int nchans = 0;
         int nsamps = 0;
         if ( potentialPaths2Data.size() == 0 ) {
@@ -56,15 +55,15 @@ int main(int argc, char const *argv[])
             return 1;
         }
         else if ( potentialPaths2Data.size() == 1 ) {
-            path2data2export = potentialPaths2Data[0];
+            path2data = potentialPaths2Data[0];
             data.getDataSpaceDimensions(path2data, nsamps, nchans);
         }
         else {
             for ( const auto & thisPath : potentialPaths2Data ) {
                 int these_nsamps, these_nchans;
-                data.getDataSpaceDimensions(path2data, these_nsamps, these_nchans);
+                data.getDataSpaceDimensions(thisPath, these_nsamps, these_nchans);
                 if ( these_nsamps > nsamps ) {
-                    path2data2export = thisPath;
+                    path2data = thisPath;
                     nsamps = these_nsamps;
                     nchans = these_nchans;
                 }
@@ -77,7 +76,8 @@ int main(int argc, char const *argv[])
         params.m_end_time = nsamps;
 
         auto output_fname = this_file.substr(0,this_file.find_first_of('.')) + ".dat";
-        data.ExportData(path2data, output_fname, params);
+        output_files.push_back(output_fname);
+        // data.ExportData(path2data, output_fname, params);
     }
     return 0;
 }
